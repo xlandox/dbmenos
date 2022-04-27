c966b759d6e3bd4ac5d5576d6507ccaca28e4e32
Orlando <xlandoxrojasx@gmail.com>
2022-04-27 12:11:43 -0500

    Se optimiza el navbar estilo hamburgesa

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        <nav class="navbar navbar-light bg-primary">
+        <nav class="navbar bg-primary navbar-expand-md navbar-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="../assets/logo-header.png" alt="logo" class="logo">
-                </a>
-                <div class="d-flex collap">
-                    <a class="nav-link" href="#">Blog</a>
-                    <button type="button" class="btn btn-info">Iniciar Sesion</button>
+                <img src="../assets/logo-header.png" alt="logo" class="logo">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse derecha" id="navbarSupportedContent">
+                    <div class="alinear">
+                        <div>
+                            <a class="link" href="#">Blog</a>
+                        </div>
+                        <div>
+                            <button type="button" class="btn btn-info">Iniciar Sesion</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
@@ -19,19 +26,38 @@
             width: 40%;
             height: auto;
         }
-        div.collap{
-            display: none !important;    
+        a.link{
+            text-decoration: none;
+        }
+        a.link,
+        button{
+            color: white;
+            font-weight: 500;
+            font-size: 20px;
         }
     }
     @media (min-width: 992px){
         img.logo{
-            width: 40%;
+            width: 20%;
             height: auto;
         }
-        a,button{
+        a.link,
+        button{
             color: white;
             font-weight: 500;
             font-size: 20px;
+            margin: 0 5px
+        }
+        a.link{
+            text-decoration: none;
+        }
+        .alinear{
+            display: flex;
+            align-items: center;
+        }
+        .derecha{
+            display: flex;
+            justify-content: flex-end;
         }
     }
 </style>
